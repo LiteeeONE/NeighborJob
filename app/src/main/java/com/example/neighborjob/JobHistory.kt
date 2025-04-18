605860cf7acfb969c946b7ae1cfc0cc2b4800a86
LiteeeONE <ulloa4206900@gmail.com>
2025-04-17 21:58:44 -0400

    Cambiar pantalla Job History a Create Job Main

--- app/src/main/java/com/example/neighborjob/JobHistory.kt
+++ app/src/main/java/com/example/neighborjob/JobHistory.kt
@@ -1,6 +1,8 @@
 package com.example.neighborjob
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,15 @@ class JobHistory : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_jobhistory)
+
+        val btnAddJob = findViewById<Button>(R.id.btnAddJob)
+
+        btnAddJob.setOnClickListener {
+            val intent = Intent(this, CreateJobMain::class.java)
+            startActivity(intent)
+
+        }
+
 
     }
 }
